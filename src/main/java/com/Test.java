820296b76ba937b601ddfbc877aaab9b61ea7dd3
zhuyiguo <zhuyiguo@didichuxing.com>
2016-07-15 18:42:34 +0800

    addTest

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author zhuyiguo
+ * @since 2016.07.15 18:38
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("add test class");
+    }
+}
